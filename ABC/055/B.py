68886dbe58fdf659ad61c6a05dab25d917611e7e
sakeharami <sakeharami@gmail.com>
2017-02-21 23:37:57 +0900

    Solve ABC 55 B 2th.

--- ABC/055/B.py
+++ ABC/055/B.py
@@ -1,15 +1,15 @@
 n = int(input())
-
-
+ 
+ 
 mod_num = pow(10, 9) + 7
-
-
+ 
+ 
 def main(n):
-    pre = 1
-    for i in range(2, n + 1):
-        pre = pre * i % mod_num
-    return pre
-
-
-ans = main(n)
-print(ans)
+    ret = 1
+    for i in range(1, n + 1):
+        ret = ret * i % mod_num
+    return ret
+    
+ 
+answer = main(n)
+print(answer)
