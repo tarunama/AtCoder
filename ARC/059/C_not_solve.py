b3285f0fcf0d0a8af2d0c5bbe6f7c4acf2f2dbe5
sakeharami <sakeharami@gmail.com>
2017-04-08 15:24:40 +0900

    Add ARC 059 C not solve.

---
+++ ARC/059/C_not_solve.py
@@ -0,0 +1,21 @@
+def main(n, ns):
+    if len(set(ns)) == 1:
+        return 0
+    
+    min_v  = min(ns)
+    max_v  = max(ns)
+    answer = pow(100, 2)
+    for i in range(min_v, max_v + 1):
+        cost = 0
+        for j in ns:
+            if i == j:
+                continue
+            cost += pow((j - i), 2)
+        answer = min(answer, cost)
+    return answer
+ 
+ 
+if __name__ == '__main__':
+    n = int(input())
+    ns = [int(e) for e in input().split()]
+    print(main(n, ns))
